1f92759da19fea7653ad33848519af02d0807334
vinoku <vinoku78@gmail.com>
2021-12-31 02:41:05 +0100

    finish part 1

--- src/app/components/hero-table/hero-table.component.ts
+++ src/app/components/hero-table/hero-table.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero, HeroService } from '../../services/hero.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'rx-hero-table',
     templateUrl: './hero-table.component.html',
     styleUrls: ['./hero-table.component.scss'],
 })
-export class HeroTableComponent implements OnInit {
-    heroes: Hero[];
+export class HeroTableComponent {
+    heroes$: Observable<Hero[]> = this.hero.heroes$;
 
-    constructor(public hero: HeroService) {
-        hero.heroes$.subscribe(heroes => {
-            this.heroes = heroes;
-        });
+    constructor(public hero: HeroService) {}
+
+    doSearch(event: any) {
+        this.hero.searchBS.next(event.target.value);
     }
-
-    ngOnInit() {}
 }
